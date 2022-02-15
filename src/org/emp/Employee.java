cd607074bc68124931ef4c1fdd5653a988075636
KasthuriManojkumar <kasthurimanojkumar2019@gmail.com>
2022-02-15 12:28:26 +0530

    Started

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,38 @@
+package org.emp;
+
+public class Employee {
+	private void empid() {
+		System.out.println("567");
+	}
+
+	private void empName() {
+		System.out.println("Navaneethan");
+	}
+
+	private void empDob() {
+		System.out.println("12/11/94");
+	}
+
+	private void empPhone() {
+		System.out.println("8110084465");
+	}
+
+	private void empEmail() {
+		System.out.println("navaneethakrishnangopal54@gmail.com");
+	}
+
+	private void empAddress() {
+		System.out.println("614,tindivanam");
+	}
+	public static void main(String[] args) {
+		Employee f=new Employee();
+		f.empid();
+		f.empName();
+		f.empDob();
+		f.empPhone();
+		f.empEmail();
+		f.empAddress();
+		
+		}
+	
+}
